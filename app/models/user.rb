8d461f2a1c07e8cf6b8591b554e562df4547c3c6
josnunezg <josnunezg@gmail.com>
2018-09-04 21:37:56 -0300

    add enum and is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+
+  enum user_type: [:normal, :admin]
+
+  def is_admin
+    self.admin?
+  end
 end
