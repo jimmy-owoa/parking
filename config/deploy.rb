c8a85428e8e147914b444bba7031d9c641661762
Jimmy <jimmyguionbajo@gmail.com>
2018-10-19 15:50:35 -0300

    remove scm

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,6 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :scm, "git"
 set :rbenv_ruby, '2.5.1'
 
 
