533115f020e841e68e9ca4ca6a1d5305807c197d
Jimmy <jimmyguionbajo@gmail.com>
2018-10-23 12:22:11 -0300

    v 1.0 materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,20 +16,6 @@
 //= require materialize
 //= require_tree .
 
-// $(document).ready(function(){
-//   $('input.autocomplete').autocomplete({
-//     data: gon.plates,
-//     limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-//     onAutocomplete: function(val) {
-//        // Callback function when value is autcompleted.
-//        location.href = gon.path;
-//
-//     },
-//     minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-//   });
-// });
-
-
 $(document).ready(function(){
   $('input.autocomplete').autocomplete({
     data: gon.plates,
