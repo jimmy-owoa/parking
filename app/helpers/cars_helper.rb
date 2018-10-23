dc45adb83cfca7f40237c0e371c70330c2cb4ea9
Jose Nuñez <josenunez@Joseph-MacBook-Pro.local>
2018-10-22 21:44:40 -0300

    create modal for create car

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,15 @@
 module CarsHelper
+
+  def header_modal_form
+    t("helpers.actions.#{params[:action]}", model: Car.model_name.human)
+  end
+
+  def owner_options
+    Owner.all.map{|i| [i.name + ' '+i.lastname, i.id]}
+  end
+
+  def brand_options
+    Brand.all.map{|i| [i.name, i.id]}
+  end
+
 end
